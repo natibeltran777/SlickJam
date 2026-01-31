141529a5c834b47f62b4980fc6e24c257aaa0434
Natalia <natibeltran1997@gmail.com>
2026-01-30 19:20:16 -0800

    Initial project submitted
    First submission with a gym and a character. Game can start and character can be slid back and forth with a and d. Press shift to Drift but the camera won't follow yet.

---
+++ LevelScenes/MainCharacter.gd
@@ -0,0 +1,81 @@
+extends CharacterBody3D
+
+
+@export var input_manager : InputManager
+@export var character_model: MeshInstance3D
+
+@export var DriftCamera : PhantomCamera3D
+@export var OnRailsCamera : PhantomCamera3D
+
+@export var SPEED = 5.0
+@export var DRIFTSPEED = 1.0
+@export var DRIFT_BOOST_SPEED = 1.0
+@export var JUMP_VELOCITY = 4.5
+@export var MAX_TURNING_MAGNITUDE = 5.0
+@export var DRIFTING_RATE = 0.5
+
+var accumulated_drift_direction = Vector3(0,0,0)
+
+var trick_in_progress = false
+
+var rotation_accumulatior = 0
+var drift_boost_active = false
+var draft_boost_max_duration = .7
+var active_boost_time = 0
+
+
+func _process(delta:float) -> void:
+	if drift_boost_active:
+		SPEED = SPEED + DRIFT_BOOST_SPEED*((draft_boost_max_duration - active_boost_time)/draft_boost_max_duration)
+		active_boost_time += delta
+		if active_boost_time >= draft_boost_max_duration:
+			drift_boost_active = false
+			active_boost_time = 0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	
+		
+			
+	if is_on_floor_only():
+		trick_in_progress = false
+		character_model.rotation.y = PI/2
+		rotation_accumulatior = 0
+	# Handle jump.
+	if input_manager._is_jump_pressed():
+		velocity.y = JUMP_VELOCITY
+		trick_in_progress = true
+		
+	if trick_in_progress:
+		rotation_accumulatior += delta*17
+		character_model.rotation.y = rotation_accumulatior
+
+		
+	var steer_input = -input_manager._get_axis()
+		
+	character_model.rotation.x = steer_input * 0.2
+		
+	if input_manager._is_drift_pressed():
+		#DriftCamera.set_priority(1)
+		#OnRailsCamera.set_priority(0)
+		var drift_direction = global_basis.z
+		rotation.y = rotation.y + (steer_input*delta*DRIFTING_RATE)
+		velocity = drift_direction * DRIFTSPEED	
+	elif input_manager._is_drift_released():
+		#DriftCamera.set_priority(0)
+		#OnRailsCamera.set_priority(1)
+		SPEED = DRIFT_BOOST_SPEED
+	else:
+		var horizontal_movement = steer_input * global_basis.x
+		
+		var velocity_cruising_magnitude = (SPEED * global_basis.z + horizontal_movement * MAX_TURNING_MAGNITUDE)
+		velocity_cruising_magnitude.y = velocity.y
+		velocity = velocity_cruising_magnitude
+	
+
+	
+	
+	move_and_slide()
---
+++ Managers/input_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+class_name InputManager
+
+
+func _is_drift_pressed() -> bool:
+	return Input.is_action_pressed("drift_pressed");
+	
+func _is_drift_released() -> bool:
+	return Input.is_action_just_released("drift_pressed");
+
+func _is_jump_pressed() -> bool:
+	return Input.is_action_just_pressed("jump_pressed");
+	
+func _is_trick_pressed() -> bool:
+	return Input.is_action_just_pressed("trick_pressed");
+	
+func _get_axis() -> float:
+	return Input.get_axis("left_pressed", "right_pressed");
