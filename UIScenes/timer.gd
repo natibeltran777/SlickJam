05df0b18ea854afc2809e788677e4e6a696af27d
Anna <anna.parmentier@gmail.com>
2026-02-08 10:50:52 -0800

    timer min var rename

--- UIScenes/timer.gd
+++ UIScenes/timer.gd
@@ -16,7 +16,7 @@ func time_to_string() -> String:
 	#Turn the time var into a string for UI display
 	var msec = fmod(time, 1) * 1000
 	var sec = fmod(time, 60)
-	var min = time / 60
+	var mins = time / 60
 	var format_string = "%02d : %02d : %02d"
-	var actual_string = format_string % [min, sec, msec]
+	var actual_string = format_string % [mins, sec, msec]
 	return actual_string
